f27234d9807c9c01c4f0a532f7182ff2c62872ba
elchincode <elchinai@code.edu.az>
2023-01-26 15:55:32 +0400

    Inheritance

---
+++ Inheritance/ExpertDocumentProgram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class ExpertDocumentProgram : ProDocumentProgram   
+    {
+
+       
+  
+
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Document Saved in pdf format");
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class HomeWork
+    {
+
+        static void Main(string[] args)
+        {
+            Console.Write("choose one of this keywords : basic , pro , expert : ");
+            Start: string keyword = Console.ReadLine();
+            
+
+            
+            switch (keyword)
+            {
+                case  "basic":
+                    DocumentProgram basic = new DocumentProgram();
+
+                    basic.OpenDocument();
+                    basic.SaveDocument();
+                    basic.EditDocument();
+
+                    break;
+
+                case "pro":
+                    ProDocumentProgram pro = new ProDocumentProgram();
+
+                    pro.OpenDocument();
+                    pro.EditDocument();
+                    pro.SaveDocument();
+
+                    break;
+
+                case "expert":
+                    ExpertDocumentProgram expert = new ExpertDocumentProgram();
+
+                    expert.OpenDocument();
+                    expert.EditDocument();
+                    expert.SaveDocument();
+
+                    break;
+
+                default:
+                    goto Start;
+
+
+
+
+
+            }
+
+         
+        }
+
+    }
+
+}
+
